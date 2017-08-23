27ab7ad127515f03d70ae0bf1f9f2020123d69c2
buxingdeyu <420783162@qq.com>
2017-08-23 19:20:21 +0800

    require js test

---
+++ requireJsTest/js/test01.js
@@ -0,0 +1,38 @@
+define(['jquery','jquery-ui'],
+	function($) {
+		var isDebug = true;
+		var initFun = function() {
+		var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#autocomplete" ).autocomplete({
+			source: availableTags
+		});
+			console.info("aaa");
+		};
+		return {
+			init : initFun
+		};
+	}
+);
